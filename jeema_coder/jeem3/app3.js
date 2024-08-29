90dc7386cdb65fa6e306cc5977bc27b435fff185
mamadou doucoure <doucouresoma10@gmail.com>
2024-08-29 14:42:14 +0200

    jeemar Coders

---
+++ jeema_coder/jeem3/app3.js
@@ -0,0 +1,65 @@
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state= {
+            prenomInput : "",
+            nomInput : "",
+            emailInput : "",
+            telephoneInput : ""
+        }
+    }
+    render(){
+        return (
+            <>
+            <h1>JeemaCoders Gestion Utilisateurs</h1>
+
+            <div className = "container col-lg-6 m-auto col-12">
+            <form>
+    <div className = "row">
+    <div class="col-6 mb-3">
+    <label class="form-label">Prenom {this.state.prenomInput}</label>
+    <input
+     type="text"
+     class = "form-control"
+       id="prenom" 
+       value = {this.state.prenomInput}
+       onChange = {(e) => {
+        this.setState({prenomInput : e.target.value})
+       }}
+       />
+  </div>
+  <div class="col-6 mb-3">
+    <label class="form-label">Nom</label>
+    <input type="text"
+      id="nom"
+       class = "form-control"
+        value = {this.state.nomInput}
+        onChange = {(e) => {
+            this.setState({nomInput : e.target.value})
+        }}
+        />
+  </div>
+    </div>
+    <div className = "row">
+    <div class="col-6 mb-3">
+    <label class="form-label">Email</label>
+    <input
+     type="email"
+       id="email" 
+       class = "form-control"
+       value = {this.state.email}/>
+  </div>
+  <div class="col-6 mb-3">
+    <label for="exampleInputEmail1" class="form-label">Telephone</label>
+    <input type="text"  id="telephone" class = "form-control"/>
+  </div>
+    </div>
+  <button type="submit" class="btn btn-primary w-100">Submit</button>
+</form>
+            </div>
+            </>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
